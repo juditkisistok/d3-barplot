81de3e2660eaa01a4c60a04c1f829516eed82117
Judit Kisistok <judit.kisistok@gmail.com>
2026-03-20 22:05:09 +0100

    add title styling

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,26 @@ export default function App() {
     .range([0, innerWidth]);
   return (
     <div>
-      <h1>D3 ❤️ React students around the world</h1>
+      <h1>
+        D3{" "}
+        <svg
+          viewBox="0 0 24 24"
+          width="20"
+          height="20"
+          style={{
+            display: "inline",
+            verticalAlign: "middle",
+            marginBottom: "3px",
+          }}
+        >
+          <path
+            fill="#c26b5a"
+            d="M12 21.6C6.4 16 1 11.3 1 7.2 1 3.4 4.1 2 6.3 2c1.3 0 4.2.5 5.7 4.5C13.6 2.5 16.5 2 17.7 2c2.5 0
+  5.3 1.6 5.3 5.2 0 4.1-5.1 8.6-11 14.4z"
+          />
+        </svg>{" "}
+        React students around the world
+      </h1>
       <Barplot
         data={data}
         axisY={axisY}
